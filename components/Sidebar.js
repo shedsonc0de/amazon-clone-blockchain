de596d5d1025d4319963cd517d2d8bcf7dfa1727
shedsoncode <codershedrack@gmail.com>
2022-09-05 04:45:44 +0100

    update to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,48 +1,62 @@
 import Image from 'next/image'
 import React from 'react'
-import { ConnectButton } from 'web3uikit'
+// import { ConnectButton } from 'web3uikit'
+import icon from '../styles/Screenshot 2022-02-08 235521.png'
+
+const isAuthenticated = true;
+const username = 'shedrack';
 
 const Sidebar = () => {
-    const isAuthenticated = false;
+    const styles = {
+        container: 'h-full w-[300px] flex flex-col bg=[#ff] static',
+        profile: 'w-full py-16 flex flex-col justify-center items-center',
+        profilePicContainer: 'flex rounded-xl items-center justify-center',
+        profilePic: 'rounded-3xl object-cover',
+        welcome: 'text-md md-2 font-bold text-2xl text-white',
+        username: `flex items-center w-full justify-center`,
+        setNickname: `text-lg font-bold flex flex-1 items-center mt-[20px] mb-[20px] text-white`,
+        usernameInput: `bg-transparent border-white border-2 rounded-lg w-[80%] py-2 px-4 text-lg mt-[20px] placeholder:text-white focus:outline-none flex justify-center items-center text-white`,
+    }
+
   return (
-    <div className={style.container}>
-        <div className={style.profile}>
-            {
-                
-                isAuthenticated && (
-                <>
-                    <div className={style.profilePicContainer}>
-                        <Image 
-                            alt='profile'
-                            style={style.profilePic}
-                            height={100}
-                            width={100}
-                        />
-                    </div>
-                    (!userName ? (
-                        <>
-                            <div className={style.userName}>
-                                <input 
-                                type='text'
-                                placeholder='Username...'
-                                className={style.usernameInput}
-                                // value={nickname}
-                                // onChange={e => setNickname(e.target.value)}
-                                />
-                            </div>
-                            <button className={style.setNickname}
-                            onClick = {handleSetUsername}
-                            >
-                               Set Nickname 
-                            </button>
-                        </>
-                    )) : (
-                        <div>
-                            <div className={style.welcome}></div>
-                        </div>
-                    )
-                )
-            }
+    <div className={styles.container}>
+      <div className={styles.profile}>
+        {isAuthenticated && (
+          <>
+            <div className={styles.profilePicContainer}>
+              <Image
+                src={icon}
+                alt='profile'
+                className={styles.profilePic}
+                height={100}
+                width={100}
+              />
+            </div>
+            {!username ? (
+              <>
+                <div className={styles.username}>
+                  <input
+                    type='text'
+                    placeholder='Username....'
+                    className={styles.usernameInput}
+                    // value={nickname}
+                    // onChange={e => setNickname(e.target.value)}
+                  />
+                </div>
+                <button
+                  className={styles.setNickname}
+                //   onClick={handleSetUsername}
+                >
+                  Set Nickname
+                </button>
+              </>
+            ) : (
+              <div>
+                <div className={styles.welcome}>Wecome {username}</div>
+              </div>
+            )}
+          </>
+        )}
         </div>
     </div>
   )
