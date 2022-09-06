fb4a267dc73e192cf882b39f2f634ce2075d2542
shedsoncode <codershedrack@gmail.com>
2022-09-06 14:05:17 +0100

    Update AmazonCoin.sol

---
+++ smart-contract/contracts/AmazonCoin.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AmazonCoin is ERC20, Ownable {
+    constructor() ERC20("AmazonCoin", "MTK") {}
+
+    function mint(uint256 amount) public payable {
+        require(msg.value == amount * 0.0001 ether, "invalid amount of ether");
+        _mint(to, amount);
+    }
+}
